0f14e97d5c8dc35dd57b76619c893c84ebbb2bc8
joi-lightyears <kunthanhdat@gmail.com>
2023-02-15 20:53:00 +0700

    add responsive sidebar

--- project/src/components/Sidebar.jsx
+++ project/src/components/Sidebar.jsx
@@ -3,35 +3,51 @@ import {Link, NavLink} from 'react-router-dom'
 import { sidebarMenu, socialIcons } from '../data/data'
 
 function Sidebar() {
+    const handleClick =()=>{
+        const aside = document.getElementById('navi')
+        aside.classList.toggle('open-menu')
+    }
   return (
-    <aside>
-        <div className="aside-wrapper">
-            <Link to={'/'} className='picture'>
-                <div className="myself"></div>
-            </Link>
-            <ul className='side-link'>{sidebarMenu.map((link,index)=>{
-                const {text, url, icon} = link
-                return(
-                    <li key={index}>
-                        <NavLink className='link-section' to={url}>
-                            {icon}
-                            {text}
-                        </NavLink>
-                    </li>
-                )
-            })}</ul>
-            <div className="social-icon">
-                {socialIcons.map((platform,index)=>{
-                    const {icon, url} = platform
-                    return(
-                        <a href={url} target="_blank" rel="noopener noreferrer" key={index}>
-                            {icon}
-                        </a>
-                    )
-                })}
-            </div>
+    <>
+        <div className="menu-btn">
+            <input type="checkbox" onClick={handleClick}/>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
-    </aside>
+
+        <aside id='navi' >
+            <div className="aside-wrapper">
+                <Link to={'/'} className='picture'>
+                    <div className="myself"></div>
+                </Link>
+                <ul className='side-link'>{sidebarMenu.map((link,index)=>{
+                    const {text, url, icon} = link
+                    return(
+                        <li key={index}>
+                            <NavLink className={({isActive})=>{
+                                return !isActive? 'link-section': 'active-link'
+                            }} to={url}>
+                                {icon}
+                                {text}
+                            </NavLink>
+                        </li>
+                    )
+                })}</ul>
+                <div className="social-icon">
+                    {socialIcons.map((platform,index)=>{
+                        const {icon, url} = platform
+                        return(
+                            <a href={url} target="_blank" rel="noopener noreferrer" key={index}>
+                                {icon}
+                            </a>
+                        )
+                    })}
+                </div>
+            </div>
+        </aside>
+    </>
+    
   )
 }
 
